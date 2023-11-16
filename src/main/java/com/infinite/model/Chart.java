fe713cc31d62d71d7546af6761491f1b9a5c6e45
yashaswiniv <yashaswiniv@ICS-LT-32PX7G3>
2023-11-16 23:26:23 +0530

    commit1

---
+++ src/main/java/com/infinite/model/Chart.java
@@ -0,0 +1,43 @@
+package com.infinite.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//task project 
+@Entity
+@Table(name="Chart")
+public class Chart {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private int salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+
+}
